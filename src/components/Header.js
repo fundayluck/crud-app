5b1d347c6bd925b76c9b3ba0123f92897b1b94e5
jember08 <afandayul@gmail.com>
2022-03-28 10:55:00 +0700

    fix some file

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="flex justify-center bg-[#A3E4DB]">
+      <h1 className="text-3xl font-bold underline p-3">Crud App</h1>
+    </div>
+  );
+};
+
+export default Header;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,195 @@
+import { useState, useEffect } from "react";
+import Header from "../components/Header";
+
+import api from "../service/api";
+
 export default function Home() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+  const [id, setId] = useState(null);
+  const [clients, setClients] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmitCreateClient = async (e) => {
+    e.preventDefault();
+    try {
+      setIsLoading(true);
+      const data = await api.post("/clients", {
+        firstName,
+        lastName,
+        email,
+        phone,
+        address,
+      });
+      setClients([...clients, data.data]);
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <>
+      <Header />
+      <div className="grid grid-cols-2 ">
+        <div className="flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 ">
+          <div className="max-w-md w-full space-y-8 rounded-md p-[20px] bg-[#FED1EF]">
+            <h1 className="flex justify-center">Add User</h1>
+            <form
+              className="mt-8 space-y-6"
+              onSubmit={handleSubmitCreateClient}
+            >
+              <input type="hidden" name="remember" defaultValue="true" />
+              <div className="rounded-md shadow-sm -space-y-px">
+                <div>
+                  <label htmlFor="email-address" className="sr-only">
+                    First Name
+                  </label>
+                  <input
+                    id="first-name"
+                    name="first-name"
+                    type="name"
+                    autoComplete="name"
+                    required
+                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="First Name"
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="email-address" className="sr-only">
+                    Last Name
+                  </label>
+                  <input
+                    id="last-name"
+                    name="last-name"
+                    type="name"
+                    autoComplete="last-name"
+                    required
+                    className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900  focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="Last Name"
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="email-address" className="sr-only">
+                    Email
+                  </label>
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    required
+                    className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900  focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="email-address" className="sr-only">
+                    Phone
+                  </label>
+                  <input
+                    id="phone"
+                    name="phone"
+                    pattern="[0-9]{10}"
+                    type="tel"
+                    autoComplete="phone"
+                    required
+                    className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900  focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="Phone"
+                    value={phone}
+                    onChange={(e) => setPhone(e.target.value)}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="address" className="sr-only">
+                    Address
+                  </label>
+                  <input
+                    id="address"
+                    name="address"
+                    type="text"
+                    autoComplete="address"
+                    required
+                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="Address"
+                    value={address}
+                    onChange={(e) => setAddress(e.target.value)}
+                  />
+                </div>
+              </div>
+
+              <div>
+                <button
+                  type="submit"
+                  className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  Create
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div className="flex items-center justify-center py-12 px-4">
+          <table className="w-full">
+            <thead className="bg-gray-50 border-b-2 border-gray-200">
+              <tr>
+                <th className="p-3 text-sm font-semibold tracking-wide text-left">
+                  Id
+                </th>
+                <th className="p-3 text-sm font-semibold tracking-wide text-left">
+                  First Name
+                </th>
+                <th className="p-3 text-sm font-semibold tracking-wide text-left">
+                  Last Name
+                </th>
+                <th className="p-3 text-sm font-semibold tracking-wide text-left">
+                  Email
+                </th>
+                <th className="p-3 text-sm font-semibold tracking-wide text-left">
+                  Phone
+                </th>
+                <th className="p-3 text-sm font-semibold tracking-wide text-left">
+                  Address
+                </th>
+                <th className="p-3 text-sm font-semibold tracking-wide text-left">
+                  Action
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="bg-gray-50">
+                <td className="p-3 text-sm text-gray-700">1</td>
+                <td className="p-3 text-sm text-gray-700">Afan</td>
+                <td className="p-3 text-sm text-gray-700">Laksono</td>
+                <td className="p-3 text-sm text-gray-700">
+                  afandayul@gmail.com
+                </td>
+                <td className="p-3 text-sm text-gray-700">081210402702</td>
+                <td className="p-3 text-sm text-gray-700">
+                  grandwisata cluster festive garden blok ag2 no 18
+                </td>
+                <td className="flex p-3 text-sm text-gray-700">
+                  <button className="p-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
+                    edit
+                  </button>
+                  <button className="p-2 bg-red-500 text-white font-semibold rounded-lg shadow-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-400 focus:ring-opacity-75">
+                    delete
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
   );
 }
