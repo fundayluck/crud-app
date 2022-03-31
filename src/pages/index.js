0ce35a900e7fec67d5bdd3ae546974d9da634c32
jember08 <afandayul@gmail.com>
2022-04-01 00:05:59 +0700

    fix console log

--- src/pages/index.js
+++ src/pages/index.js
@@ -231,7 +231,6 @@ export default function Home() {
                   pattern="[0-9]{4}[0-9]{4}[0-9]{4}"
                   maxLength={12}
                   autoComplete="phone"
-                  required
                   className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900  focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                   placeholder="Phone eg. 081223232323"
                   value={phone}
@@ -257,7 +256,7 @@ export default function Home() {
 
             <div>
               <button
-                isloading={isLoading}
+                isloading={isLoading ? 1 : 0}
                 type="submit"
                 className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
